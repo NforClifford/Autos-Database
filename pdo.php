bb99275c53835cc9e5e05701e7f91a5aaa9ec149
NforClifford <yungongn@gmail.com>
2024-05-07 07:33:55 +0200

    Create pdo.php

---
+++ pdo.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Nfor Clifford Yungong-database connect</title>
+</head>
+<body>
+	<?php
+	$pdo = new PDO('mysql:host=localhost;port=3306;dbname=misc','king','zap'); 
+	$pdo->setAttribute(PDO::ATT_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	?>
+
+</body>
+</html>
