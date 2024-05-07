330f06210b3cae2da5fadf0e68dad93df41cf74b
NforClifford <yungongn@gmail.com>
2024-05-07 07:32:01 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Nfor Clifford Yungong - Autos Database</title>
+</head>
+<body>
+	<div class="content">
+		<h1 style="color: blueviolet;">Welcom to Autos Database</h1>
+		<p>
+			Please log in <a href="login.php" style="background-color: lightblue;">here</a> to <a href="autos.php">Autos Database </a>
+		</p>
+		
+	</div>
+</body>
+</html>
